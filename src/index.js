f103d8005528813f88db9276eea383b8c5fc9815
seankudryk <seankudryk@hotmail.ca>
2024-11-22 17:13:08 -0500

    created a new function to handle creating new todo objects, and then pushing that new created object to a list array

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-import { test } from "./test.js";
+import { test, createNewEntry, getTodoList } from "./test.js";
 
 test("test");
+
+createNewEntry("idk", "whatever", "i dont care", "none", "naw", "no thanks");
+createNewEntry(2, "whatever", "i dont care", "none", "naw", true);
+getTodoList();
--- src/test.js
+++ src/test.js
@@ -1,3 +1,28 @@
 export const test = (string) => {
     console.log(`${string} was the user input!`);
 }
+// export const sampleTask = new Todo("idk", "whatever", "i dont care", "none", "naw", "no thanks");
+const todoList = [];
+
+export const createNewEntry = (title, description, dueDate, priority, notes, checklist) => {
+
+    class Todo {
+        constructor(title, description, dueDate, priority, notes, checklist) {
+            this.title = title;
+            this.description = description;
+            this.dueDate = dueDate;
+            this.priority = priority;
+            this.notes = notes;
+            this.checklist = checklist;
+        }
+    }
+
+    // const newEntry = new Todo("idk", "whatever", "i dont care", "none", "naw", "no thanks");
+    const newEntry = new Todo(title, description, dueDate, priority, notes, checklist);
+    todoList.push(newEntry);
+}
+
+export const getTodoList = () => {
+    console.log(todoList);
+}
+
