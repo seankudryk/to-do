814d074c4e9baa0218f9c749ef3f5954810c74bc
seankudryk <seankudryk@hotmail.ca>
2024-11-23 17:20:30 -0500

    added input elements which will be used to update entry information

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import { test, createNewEntry, getTodoList } from "./test.js";
+import "./styles.css"
+import { test, createNewEntry, getTodoList, displayEntry } from "./test.js";
 
 test("test");
 
 createNewEntry("idk", "whatever", "i dont care", "none", "naw", "no thanks");
 createNewEntry(2, "whatever", "i dont care", "none", "naw", true);
 getTodoList();
+
+const submitButton = document.querySelector("#submit-button");
+
+submitButton.addEventListener("click", displayEntry);
--- src/test.js
+++ src/test.js
@@ -26,3 +26,18 @@ export const getTodoList = () => {
     console.log(todoList);
 }
 
+export const displayEntry = () => {
+    const todoDisplayDiv = document.querySelector("#to-do-display-div")
+    const titleInput = document.querySelector("#title-input").value;
+    const descriptionInput = document.querySelector("#description-input");
+    const dueDateInput = document.querySelector("#due-date-input");
+    const priorityInput = document.querySelector("#priority-input");
+    const notesInput = document.querySelector("#notes-input");
+    const checklistInput = document.querySelector("#checklist-input");
+    const newDiv = document.createElement("div");
+
+    todoDisplayDiv.appendChild(newDiv);
+
+    console.log(titleInput);
+}
+
