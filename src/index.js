3b876b3d2e0d2b27075c75b1b34b0ea754339e89
seankudryk <seankudryk@hotmail.ca>
2024-12-07 17:31:00 -0500

    editted the layout of the side bar set up a modal to appear on createNewProjectButton click event. User is prompted to enter a value (this is not functional yet).

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "./styles.css"
-import { createNewEntry, getTodoList, displayEntry } from "./test.js";
+import { createNewEntry, getTodoList, displayEntry, displayNewProject } from "./test.js";
 
 const titleInput = document.querySelector("#title-input");
 const descriptionInput = document.querySelector("#description-input");
@@ -7,8 +7,12 @@ const dueDateInput = document.querySelector("#due-date-input");
 const priorityInput = document.querySelector("#priority-input");
 const notesInput = document.querySelector("#notes-input");
 const checklistInput = document.querySelector("#checklist-input");
+const projectInput = document.querySelector("#project-input");
+const sideBar = document.querySelector("#sidebar");
 
 const submitButton = document.querySelector("#submit-button");
+const createProjectButton = document.querySelector("#create-project-button");
+const projectNameSubmitButton = document.querySelector("");
 
 const resetInputs = () => {
     titleInput.value = "";
@@ -20,8 +24,18 @@ const resetInputs = () => {
 }
 
 submitButton.addEventListener("click", () => {
-    createNewEntry(titleInput.value, descriptionInput.value, dueDateInput.value, priorityInput.value, notesInput.value, checklistInput.value);
-    getTodoList();
+    createNewEntry(titleInput.value, descriptionInput.value, dueDateInput.value, priorityInput.value, notesInput.value, checklistInput.value, projectInput.value);
     displayEntry();
     resetInputs();
 });
+
+createProjectButton.addEventListener ("click", () => {
+    const modal = document.querySelector(".modal");
+    // const projectNameSubmitButton = document.querySelector("#project-name-submit-button");
+    
+    modal.style.display = "flex";
+    // projectNameSubmitButton.addEventListener("click", () => {
+    //     modal.style.display = "none";
+    // });
+});
+
